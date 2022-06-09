8a9d916174e6812516333d5e2b5c82458c5f3cfe
Yorch82 <jcampo1982@outlook.com>
2022-06-09 19:11:42 +0200

    retoques finales

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div className='footer-container' >
+            <h2>Portfolio :</h2>
+            <a className='button' href="https://github.com/Yorch82?tab=repositories" target="_blank">My recently projects</a>
+            <footer className="App-footer">All rigths reserved. Yorch of the island ®</footer>
+        </div>       
+    )
+}
+
+export default Footer
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -19,16 +19,16 @@ const Home = () => {
         <div className='body'>
             <div>
                 <div>
-                    <img className='image' src="https://media.giphy.com/media/dWesBcTLavkZuG35MI/giphy.gif"/>
+                    <img className='image' src="https://media.giphy.com/media/dWesBcTLavkZuG35MI/giphy.gif" alt="gif"/>
                 </div>
                 <div>
                     <h2>About Me :</h2>
-                    <p>I am a Full Stack Developer Student <img src="https://media.giphy.com/media/WUlplcMpOCEmTGBtBW/giphy.gif" width="30" /> from Spain</p>
+                    <p>I am a Full Stack Developer Student <img src="https://media.giphy.com/media/WUlplcMpOCEmTGBtBW/giphy.gif" width="30" alt='cat'/> from Spain</p>
                     <ul className='list'>
                         <li>I'm currently taking bootcamp of Full Stack Web Developer @ The Bridge School, Valencia.</li>
                         <li>Full time geek.</li>
                         <li>In my free time I like to play FPS games and play guitar.</li>
-                        <li>How to reach me: <a href="https://www.linkedin.com/in/jorge-campo-silvestre-350b7b174">LinkedIn</a></li>
+                        <li>How to reach me: <a className='button' href="https://www.linkedin.com/in/jorge-campo-silvestre-350b7b174" target="_blank">LinkedIn</a></li>
                     </ul>
                 </div>                
             </div>
